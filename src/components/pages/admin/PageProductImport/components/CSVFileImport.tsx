c35a0b07495808b8b8f12c135af5d797389d129f
Viktoriya Nykytenko <kitakiv@gmail.com>
2025-03-22 18:34:28 +0200

    feat: add token for FE

--- src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
+++ src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
-import axios from "axios";
+import axios, { AxiosRequestHeaders } from "axios";
+import AlertIcon from "./alert";
 
 type CSVFileImportProps = {
   url: string;
@@ -10,6 +11,10 @@ type CSVFileImportProps = {
 
 export default function CSVFileImport({ url, title }: CSVFileImportProps) {
   const [file, setFile] = React.useState<File>();
+  const [error, setError] = React.useState<{
+    message: string;
+    statusCode: number;
+  } | null>(null);
 
   const onFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const files = e.target.files;
@@ -30,12 +35,23 @@ export default function CSVFileImport({ url, title }: CSVFileImportProps) {
         throw new Error("No file selected");
       }
       // Get the presigned URL
+      const header: AxiosRequestHeaders = localStorage.getItem(
+        "authorization_token"
+      )
+        ? {
+            Authorization: `Basic ${localStorage.getItem(
+              "authorization_token"
+            )}`,
+          }
+        : {};
+
       const response = await axios({
         method: "GET",
         url,
         params: {
           name: encodeURIComponent(file.name),
         },
+        headers: header,
       });
       console.log("File to upload: ", file.name);
       console.log("Uploading to: ", response.data);
@@ -46,22 +62,56 @@ export default function CSVFileImport({ url, title }: CSVFileImportProps) {
       console.log("Result: ", result);
       setFile(undefined);
     } catch (error) {
-      console.error("There was an error uploading the file", error);
+      console.log(error);
+      if (axios.isAxiosError(error)) {
+        setError({
+          message: (
+            (error.response as unknown as { data: { message: string } })
+              .data as { message: string }
+          ).message,
+          statusCode: (
+            (error.response as unknown as { data: { statusCode: number } })
+              .data as { statusCode: number }
+          ).statusCode,
+        });
+      } else if (error instanceof Error) {
+        if (error.message.includes("status: 401")) {
+          setError({
+            message: "Unauthorized: You are not logged in",
+            statusCode: 401,
+          });
+        } else if (error.message.includes("status: 403")) {
+          setError({
+            message:
+              "Forbidden: You do not have permission to access this resource",
+            statusCode: 403,
+          });
+        } else {
+          setError({
+            message: error.message,
+            statusCode: 500,
+          });
+        }
+      }
+      console.error("Upload error:", error);
     }
   };
   return (
-    <Box>
-      <Typography variant="h6" gutterBottom>
-        {title}
-      </Typography>
-      {!file ? (
-        <input type="file" onChange={onFileChange} />
-      ) : (
-        <div>
-          <button onClick={removeFile}>Remove file</button>
-          <button onClick={uploadFile}>Upload file</button>
-        </div>
-      )}
-    </Box>
+    <>
+      <Box>
+        <Typography variant="h6" gutterBottom>
+          {title}
+        </Typography>
+        {!file ? (
+          <input type="file" onChange={onFileChange} />
+        ) : (
+          <div>
+            <button onClick={removeFile}>Remove file</button>
+            <button onClick={uploadFile}>Upload file</button>
+          </div>
+        )}
+      </Box>
+      {error && <AlertIcon {...error} />}
+    </>
   );
 }
--- src/components/pages/admin/PageProductImport/components/alert.tsx
+++ src/components/pages/admin/PageProductImport/components/alert.tsx
@@ -1,14 +1,29 @@
 import { Alert, AlertTitle } from "@mui/material";
+import React from "react";
 
 export default function AlertIcon(error: {
   message: string;
   statusCode: number;
 }) {
+  const [open, setOpen] = React.useState(true);
   return (
-    // eslint-disable-next-line @typescript-eslint/no-empty-function
-    <Alert severity="error" onClose={() => {}}>
-      <AlertTitle>Error {error.statusCode}</AlertTitle>
-      {error.message}
-    </Alert>
+    open && (
+      <Alert
+        style={{
+          position: "fixed",
+          top: 10,
+          right: 20,
+          zIndex: 10,
+          width: 300,
+        }}
+        severity="error"
+        onClose={() => {
+          setOpen(false);
+        }}
+      >
+        <AlertTitle>Error {error.statusCode}</AlertTitle>
+        {error.message}
+      </Alert>
+    ) || <></>
   );
 }
