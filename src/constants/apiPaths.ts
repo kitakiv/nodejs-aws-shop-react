c16ee5ced09e700cba49dcbb361831a7d1dd08d9
Viktoriya Nykytenko <kitakiv@gmail.com>
2025-04-14 15:02:59 +0300

    feat: change link to cart and order

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
   product: "https://im1sioaeng.execute-api.eu-west-1.amazonaws.com/dev",
-  order: "https://d35gdn4vt0ujr8.cloudfront.net/",
+  order: "http://nodejs-aws-cart-api-9-dev.eu-west-1.elasticbeanstalk.com/",
   import: "https://xj38d7qlm1.execute-api.eu-west-1.amazonaws.com/prod/",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  cart: "https://d35gdn4vt0ujr8.cloudfront.net/api",
+  cart: "http://nodejs-aws-cart-api-9-dev.eu-west-1.elasticbeanstalk.com/api",
 };
 
 export default API_PATHS;
